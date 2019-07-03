4f844639f7b27cd5d656dfc8559a0983e3c4d602
Xavier Gomez <xendke@gmail.com>
2019-07-02 20:40:12 -0400

    make the front end prettier

--- app/js/utils.js
+++ app/js/utils.js
@@ -9,8 +9,9 @@ const scoresElement = document.getElementById('scores');
 if(!scoresElement) throw "canvas does not exits.";
 
 const updateScore = (data) => {
-	if(scoresElement.innerHTML !== `Player One: ${data.p1.score} || Player Two: ${data.p2.score}`) {
-		scoresElement.innerHTML = `Player One: ${data.p1.score} || Player Two: ${data.p2.score}`;
+	const template = `<span>${data.p1.score}</span><span>VS</span><span>${data.p2.score}</span>`
+	if(scoresElement.innerHTML !== template) {
+		scoresElement.innerHTML = template;
 	}
 }
 
